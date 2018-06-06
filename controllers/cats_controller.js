f929b46ae3c2e165a996cabed37e5bb9db9fc533
Kitty <kittykumas@gmail.com>
2018-06-05 22:43:05 -0700

    draft version of orm.js and cat_controller done, but skipped the model cat.js for now;updated the css theme to fix cat rescue site; twick index.handlebar to render cats correctly and add some buttons; now adpot cat button works; putting method on router is hard

--- controllers/cats_controller.js
+++ controllers/cats_controller.js
@@ -5,21 +5,40 @@ var router = express.Router();
 var cat = require("../models/cat.js");
 
 //testing data base connection
-var connection = require("../config/connection.js")
-router.get("/",function(req,res){
-    connection.query("SELECT * FROM cats;",function(err,data){
-        if(err) throw err;
-        console.log(data);
-        res.json(data);
-    })
-})
-
-// Create all routes and set up logic within those routes where required.
+// var connection = require("../config/connection.js")
 // router.get("/",function(req,res){
 //     connection.query("SELECT * FROM cats;",function(err,data){
 //         if(err) throw err;
 //         console.log(data);
+//         res.json(data);
 //     })
 // })
 
+var orm = require("../config/orm.js")
+// Create all routes and set up logic within those routes where required.
+router.get("/",function(req,res){
+    orm.selectAll("cats",function(data){
+        // console.log(data);
+        // res.json(data);
+        res.render("../views/index.handlebars",{cats:data})
+    });
+})
+
+router.post("/addcat",function(req,res){
+    orm.insertOne("cats","cat_name,rescued",[req.body.name,req.body.rescued],function(data){
+        // console.log(data);
+        res.json(data);
+
+    });
+})
+
+router.put("/adpotcat/:id",function(req,res){
+     console.log("inside adopt");
+
+    orm.update("cats",{rescued:req.body.rescued},{id:req.params.id},function(data){
+        // console.log(data);
+        res.json(data);
+
+    });
+})
 module.exports = router;
